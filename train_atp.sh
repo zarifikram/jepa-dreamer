ca38da78396ae02597f36830f36f46fedc3021cc
chrm <chrm@renyi.hu>
2024-09-28 19:39:58 +0000

    wip

--- train_atp.sh
+++ train_atp.sh
@@ -1,4 +1,4 @@
-game_name=crazy_climber
+game_name=asterix
 type=bottleneck_atc
 proj_name=ablation
 exp_name=atari_${game_name}
@@ -8,11 +8,10 @@ use_extra_loss=True
 # run it for three seeds concurrently 
 #mkdir logdir
 
-for seed in 666
+for seed in 0 6 42
 do
-    nohup python dreamer.py --task=atari_${game_name} --seed ${seed} --use_extra_loss ${use_extra_loss} --wandb_proj ${proj_name} --wandb_exp ${exp_name}_seed${seed} --configs atari100k updates atp debug --logdir ./logdir/${game_name}_${type}_seed${seed} &> ./logdir/${game_name}_${type}_seed${seed}.log 2> ./logdir/${game_name}_${type}_seed${seed}.err &
+    nohup python dreamer.py --task=atari_${game_name} --seed ${seed} --use_extra_loss ${use_extra_loss} --wandb_proj ${proj_name} --wandb_exp ${exp_name}_seed${seed}_use_extra_loss${use_extra_loss} --configs atari100k updates atp debug --logdir ./logdir/${game_name}_${type}_seed${seed} &> ./logdir/${game_name}_${type}_seed${seed}.log 2> ./logdir/${game_name}_${type}_seed${seed}.err &
 done
-
  
 # python dreamer.py --task=atari_frostbite --configs atari100k updates icm --logdir ./logdir/atari-icm
  
